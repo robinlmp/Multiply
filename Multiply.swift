69b112d420233ab7c513ccdfa63247fcc247bd0c
robinlmp <53621609+robinlmp@users.noreply.github.com>
2021-06-16 11:35:29 +0100

    Create Multiply.swift

---
+++ Multiply.swift
@@ -0,0 +1,16 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
